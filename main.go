9d0336375e16e517a88203b5b32fb177e908db8a
Noor Mohammed <mail@noormohammed.tech>
2021-11-06 21:46:04 +0530

    read from database

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -15,50 +17,66 @@ func main() {
 	}
 	defer db.Close()
 
-	insertData(db)
-	// showPeaples(db)
+	// insertData(db)
+	dataFromDb, _ := showPeaples(db)
+	fmt.Println(dataFromDb)
 
-}
-func insertData(db *sql.DB) error {
-	peaple := GetData()
-	fmt.Println(peaple)
-	for _, person := range peaple {
-		dbQuery := "INSERT INTO person(name, age, location) VALUES(?, ?, ?)"
-		insertQuery, err := db.Prepare(dbQuery)
-		if err != nil {
-			fmt.Println("insert query prepare error")
-			return err
-		}
-		defer insertQuery.Close()
+	jsonData, _ := json.MarshalIndent(dataFromDb, "", " ")
 
-		dbResponse, err := insertQuery.Exec(person.Name, person.Age, person.Location)
-		if err != nil {
-			return err
-		}
-		fmt.Println(dbResponse)
-	}
-	return nil
+	fmt.Println(string(jsonData))
 
 }
 
-// func showPeaples(db *sql.DB) {
-// 	dbQuery := "SELECT * FROM person"
-// 	selectQuery, _ := db.Prepare(dbQuery)
-// 	_, err := selectQuery.Exec()
-// 	if err != nil {
-// 		panic(err)
+// func insertData(db *sql.DB) error {
+// 	peaple := GetData()
+// 	fmt.Println(peaple)
+// 	for _, person := range peaple {
+// 		dbQuery := "INSERT INTO person(name, age, location) VALUES(?, ?, ?)"
+// 		insertQuery, err := db.Prepare(dbQuery)
+// 		if err != nil {
+// 			fmt.Println("insert query prepare error")
+// 			return err
+// 		}
+// 		defer insertQuery.Close()
+
+// 		dbResponse, err := insertQuery.Exec(person.Name, person.Age, person.Location)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		fmt.Println(dbResponse)
 // 	}
-// 	var per []Person
-// 	defer selectQuery.Close()
-// 	selectQuery.QueryRow().Scan(&per)
-// 	fmt.Printf("%#v", selectQuery)
-// 	fmt.Println(per)
-// 	// for dbResults {
-// 	// 	var row Person
-// 	// 	err := dbResults.Scan(&row)
-// 	// 	if err != nil {
-// 	// 		panic(err)
-// 	// 	}
-// 	// 	fmt.Println(row)
-// 	// }
+// 	return nil
+
 // }
+
+func showPeaples(db *sql.DB) ([]Person, error) {
+	dbQuery := "SELECT * FROM person"
+	selectQuery, err := db.Query(dbQuery)
+	if err != nil {
+		panic(err)
+	}
+	var peaple []Person
+	defer selectQuery.Close()
+	for selectQuery.Next() {
+		var iterPerson Person
+
+		err := selectQuery.Scan(&iterPerson.Id, &iterPerson.Name, &iterPerson.Age, &iterPerson.Location)
+		if err != nil {
+			log.Fatal(err)
+			return peaple, err
+		}
+
+		peaple = append(peaple, iterPerson)
+
+	}
+	// for dbResults {
+	// 	var row Person
+	// 	err := dbResults.Scan(&row)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(row)
+	// }
+
+	return peaple, nil
+}
